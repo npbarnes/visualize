f756852623e3e8b5ce58289d08aa958cef0fee44
Nathan Barnes <npbarnes@alaska.edu>
2015-08-28 11:24:31 -0800

    Adds support for vector quantities.

--- hybridReader.py
+++ hybridReader.py
@@ -20,35 +20,59 @@ class hybridReader:
 
     def _getDatalst(self):
         # taken in reversed order to make them go from top to bottom.
-        return [self._grabData("data/c.np_3d_"+str(i)+".dat") for i in reversed(range(11,11+self.numProc))]
+        return np.array([self._grabData("data/"+self.filename+str(i)+".dat") 
+                            for i in reversed(range(11,11+self.numProc))])
 
     def _getTimesteps(self):
         return len(self.datalst[0])
 
 
-    # TODO: This function is very inefficient, make the whole thing in numpy arrays.
+    # TODO: This function is very inefficient, make it faster.
+    # TODO: Eliminate code duplication.
     def shapeData(self):
-        ret = []
-        for i in range(self.timesteps):
-            tmp = []
-            for proc in self.datalst:
-                tmp.append(proc[i][:-2*self.nx*self.ny])
-            # flatten
-            tmp = [val for xyz in tmp for val in xyz]
-            # reshape to 3-d grid
-            tmp = np.reshape(tmp, [self.nx,self.ny,self.zrange],'F').tolist()
-            # timestep is ready to be returned
-            ret.append(tmp)
+        if(self.isVec):
+            ret = [[],[],[]]
+            # Roll vector coordinate axis to the space before cell x coordinate.
+            redatalst = np.reshape(self.datalst,[self.numProc,self._getTimesteps(),self.nx,self.ny,self.nz,3], 'F')
+            rolledlst = np.rollaxis(redatalst,5,2)
+            for i in range(self.timesteps):
+                for c in range(3):
+                    tmp = []
+                    for proc in rolledlst:
+                        tmp.append(proc[i,c,:,:,:-2])
+                    # flatten
+                    tmp = np.reshape(tmp,[self.nx,self.ny,self.numProc*self.nz])
+                    # timestep is ready to be returned
+                    ret[c].append(tmp)
+            #put it in the correct order (t,x,y,z,c)
+            #TODO: fix this mixup with np arrays and lists.
+            #      There's no reason to convert to a list and then right back.
+            ret = np.rollaxis(np.array(ret),0,5).tolist()
+        else:
+            ret = []
+            for i in range(self.timesteps):
+                tmp = []
+                for proc in self.datalst:
+                        tmp.append(proc[i][:-2*self.nx*self.ny])
+                # flatten
+                tmp = [val for xyz in tmp for val in xyz]
+                # reshape to 3-d grid
+                tmp = np.reshape(tmp, [self.nx,self.ny,self.zrange],'F').tolist()
+                # timestep is ready to be returned
+                ret.append(tmp)
+
         return np.array(ret)
 
     def _get3dData(self):
         return self.shapeData()
 
-    def __init__(self,nx,ny,nz,numProc):
+    def __init__(self,filename,nx,ny,nz,numProc,isVec=False):
+        self.filename = filename
         self.nx = nx
         self.ny = ny
         self.nz = nz
         self.numProc = numProc
+        self.isVec = isVec
         self.zrange = (nz-2)*numProc
         self.datalst = self._getDatalst()
         self.timesteps = self._getTimesteps()
