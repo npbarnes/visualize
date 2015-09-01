d7cbe7d93cc14881f29826e14e99f54433f7695e
Nathan Barnes <npbarnes@alaska.edu>
2015-09-01 14:07:27 -0800

    Corrects leftover debug test to use real data.

--- density.py
+++ density.py
@@ -29,7 +29,7 @@ density = plt.imshow(getSlice(t0,s0),interpolation='nearest',origin='lower')
 plt.colorbar()
 
 xcomp, ycomp = getProjection(t0,s0)
-Bfield = plt.quiver(np.arange(60),np.arange(ny),np.zeros(60,ny),ycomp, color='white')
+Bfield = plt.quiver(np.arange(60),np.arange(ny),xcomp,ycomp, color='white')
 
 # Setup UI
 # TODO: Play/Pause button
