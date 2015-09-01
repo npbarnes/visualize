16d710e1311dfba8a7c7e6ec9a7e43a908b13d14
Nathan Barnes <npbarnes@alaska.edu>
2015-09-01 11:46:30 -0800

    Labels Density reader as such.

--- density.py
+++ density.py
@@ -10,9 +10,10 @@ ny = 59
 nz = 13
 numProc = 4
 
-reader = hr.hybridReader(nx,ny,nz,numProc)
-zrange = reader.zrange
-getSlice = reader.getSlice
+
+Dreader = hr.hybridReader("c.np_3d_",nx,ny,nz,numProc)
+zrange = Dreader.zrange
+getSlice = Dreader.getSlice
 
 # Set initial slice and time
 s0 = 'xy'
@@ -21,7 +22,7 @@ t0 = 0
 
 # Make initial plot
 fig, ax = plt.subplots()
-obj = plt.imshow(getSlice(t0,s0),interpolation='nearest',origin='lower') 
+density = plt.imshow(getSlice(t0,s0),interpolation='nearest',origin='lower') 
 plt.colorbar()
 
 # Setup UI
@@ -32,26 +33,28 @@ rax = plt.axes([0.01,0.7,0.07,0.15])
 radio = RadioButtons(rax, ('xy','xz','yz'))
 
 axtime = plt.axes([0.25,0.03,0.65,0.03])
-stime = Slider(axtime, 'Time', 0, 19, valinit=t0)
+stime = Slider(axtime, 'Time', 0, 34, valinit=t0)
 
 # UI update functions
 def radioFunc(s):
     global s0 
     s0 = s
-    obj.set_data(getSlice(t0,s0))
+    density.set_data(getSlice(t0,s0))
     if(s0 == 'xy'):
-        obj.set_extent([0,60,0,ny])
+        density.set_extent([0,60,0,ny])
     elif(s0 == 'xz'):
-        obj.set_extent([0,60,0,zrange])
+        density.set_extent([0,60,0,zrange])
     elif(s0 == 'yz'):
-        obj.set_extent([0,ny,0,zrange])
+        density.set_extent([0,ny,0,zrange])
     plt.draw()
 radio.on_clicked(radioFunc)
 
 def update(val):
     global t0 
     t0 = int(stime.val)
-    obj.set_data(getSlice(t0,s0))
+
+    density.set_data(getSlice(t0,s0))
+
     plt.draw()
 stime.on_changed(update)
 
