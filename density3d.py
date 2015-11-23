ef99f023794a9185366639a34f77fa081e5fd135
Nathan Barnes <npbarnes@alaska.edu>
2015-11-23 14:06:48 -0900

    All the density3d.py changed that I never commited.

--- density3d.py
+++ density3d.py
@@ -7,6 +7,8 @@ from mpl_toolkits.mplot3d import axes3d
 #import matplotlib.animation as animation
 from matplotlib.widgets import Slider, RadioButtons
 import hybridReader as hr
+import matplotlib.cm as cm
+from matplotlib.colors import Normalize
 
 nx = 119
 ny = 59
@@ -36,7 +38,7 @@ def findPoints(t,threshold):
 
 # Set initial slice and time
 s0 = 'xy'
-t0 = 0
+t0 = 30
 
 fig = plt.figure()
 ax = fig.add_subplot(111,projection='3d')
@@ -44,21 +46,32 @@ ax = fig.add_subplot(111,projection='3d')
 ave = findAveDensity()
 numframes = len(data)
 pointslst = [findPoints(t, ave*3.5) for t in range(numframes)]
-x,y,z = zip(*pointslst[0])
-sc, = ax.plot(x,y,z,'b,')
+xs,ys,zs = zip(*pointslst[t0])
+thing = zip(xs,ys,zs)
 
-def update(num):
-    global t0
-    t0 = t0+1
-    x,y,z = zip(*pointslst[num])
-    # Can't set_data in 3-d so instead set 2-d data then
-    # use set_3d_properties to add z component
-    sc.set_data(x,y)
-    sc.set_3d_properties(z)
-    print(num)
-    return sc,
 
-ani = animation.FuncAnimation(fig, update, frames=numframes, interval=50, blit=False)
+colors = [data[t0][x][y][z] for x,y,z in thing]
+norm = Normalize(vmin=min(colors),vmax=max(colors))
+cmap = cm.hot
+m = cm.ScalarMappable(norm=norm, cmap=cmap)
+rgbacolors = [m.to_rgba(c) for c in colors]
+rgbcolors = [(r,g,b) for (r,g,b,a) in rgbacolors]
+
+print(len(xs),len(ys),len(zs),len(rgbcolors))
+
+sc, = ax.plot(xs,ys,zs,c='red', marker = 'o', linestyle='None')
+
+#def update(num):
+#    global t0
+#    t0 = t0+1
+#    x,y,z = zip(*pointslst[num])
+#    # Can't set_data in 3-d so instead set 2-d data then
+#    # use set_3d_properties to add z component
+#    sc.set_data(x,y)
+#    sc.set_3d_properties(z)
+#    return sc,
+#
+#ani = animation.FuncAnimation(fig, update, frames=numframes, interval=50, blit=False)
 
 
 plt.show()
