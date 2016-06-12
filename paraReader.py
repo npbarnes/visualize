d75850475b39ca00ad0cf3f58f49571bd42e48ce
Nathan Barnes <npbarnes@alaska.edu>
2016-06-12 15:04:11 -0800

    Updates paraReader to the new FortranFile.
    Also improves printing and takes the folder from the command line.

--- paraReader.py
+++ paraReader.py
@@ -1,27 +1,58 @@
 #!/usr/bin/python
 import FortranFile as ff
 
-para = ff.FortranFile('tmp/para.dat')
+from os.path import join
+import numpy as np
+from pprint import pprint
+import sys
 
-one = para.readStuff(['i','i','i','f','f','f'])
-two = para.readStuff(['i','f','i','f','i'])
-out_dir = para.readString()
-three = para.readReals()
-Ni_max = para.readInts()
-masses = para.readStuff(['d','d','f'])
-densities = para.readReals()
-fields = para.readReals()
-thermal = para.readReals()
-alpha, beta = para.readStuff(['d','f'])
-RIo = para.readReals()
-print 'nx,ny,nz,dx,dy,delz',one
-print 'nt,dtsub_init,ntsub,dt,nout',two
-print 'out_dir', out_dir
-print 'vtop,vbottom', three
-print 'Ni_max', Ni_max
-print 'mproton,mpu,mheavy',masses
-print 'np_top,np_bottom',densities
-print 'b0_top,b0_bottom',fields
-print 'vth_top,vth_bottom',thermal
-print 'alpha,beta',alpha,beta
-print 'RIo',RIo
+def getParameters(prefix):
+    f = ff.FortranFile(join(prefix,'para.dat'))
+    r1 = f.readOther([  ('nx',np.int32),
+                        ('ny',np.int32),
+                        ('nz',np.int32),
+                        ('dx',np.float32),
+                        ('dy',np.float32),
+                        ('delz',np.float32)])
+    para = dict(zip(r1.dtype.names,r1[0]))
+
+    r2 = f.readOther([  ('nt',np.int32),
+                        ('dtsub_init',np.float32),
+                        ('ntsub',np.int32),
+                        ('dt',np.float32),
+                        ('nout',np.int32)])
+    para.update(zip(r2.dtype.names,r2[0]))
+
+    out_dir = f.readString()
+    para.update({'out_dir':out_dir})
+
+    r3 = f.readReals()
+    para.update([('vtop',r3[0]),('vbottom',r3[1])])
+
+    Ni_max = f.readInts()[0]
+    para.update({'Ni_max':Ni_max})
+
+    r4 = f.readOther([  ('mproton',np.float64),
+                        ('mpu',np.float64),
+                        ('mheavy',np.float32)])
+    para.update(zip(r4.dtype.names,r4[0]))
+
+    r5 = f.readReals()
+    para.update([('np_top',r5[0]),('np_bottom',r5[1])])
+
+    r6 = f.readReals()
+    para.update([('b0_top',r6[0]),('b0_bottom',r6[1])])
+
+    r7 = f.readReals()
+    para.update([('vth_top',r7[0]),('vth_bottom',r7[1])])
+
+    r8 = f.readOther([  ('alpha',np.float64),
+                        ('beta',np.float32)])
+    para.update(zip(r8.dtype.names,r8[0]))
+
+    RIo = f.readReals()[0]
+    para.update({'RIo':RIo})
+
+    return para
+
+pprint(getParameters(sys.argv[1]))
