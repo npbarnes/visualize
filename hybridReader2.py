bfe0b77725867d8537095e26f8e91325811f10f2
Nathan Barnes <npbarnes@alaska.edu>
2016-06-12 15:06:10 -0800

    Updates getparameters to the new FortranFile style

--- hybridReader2.py
+++ hybridReader2.py
@@ -11,31 +11,56 @@ class HybridReader2:
     def __init__(self, prefix, variable):
         self.prefix = prefix
         self.var = variable
-        self.para = self.getParameters()
+        self.para = self._getParameters()
 
-    def getParameters(self):
-        para = ff.FortranFile(join(self.prefix,'para.dat'))
-        one = para.readStuff(['i','i','i','f','f','f'])
-        two = para.readStuff(['i','f','i','f','i'])
-        out_dir = para.readString()
-        three = para.readReals()
-        Ni_max = para.readInts()
-        masses = para.readStuff(['d','d','f'])
-        densities = para.readReals()
-        fields = para.readReals()
-        thermal = para.readReals()
-        alpha, beta = para.readStuff(['d','f'])
-        RIo = para.readReals()
-        return {'nx':one[0],'ny':one[1],'nz':one[2],
-                'dx':one[3],'dy':one[4],'delz':one[5],
-                'nt':two[0],'dtsub_init':two[1],'ntsub':two[2],'dt':two[3],'nout':two[4],
-                'out_dir':out_dir,'vtop':three[0],'vbottom':three[1],
-                'Ni_max':Ni_max,'mproton':masses[0],'mpu':masses[1],'mheavy':masses[2],
-                'np_top':densities[0],'np_bottom':densities[1],
-                'b0_top':fields[0],'b0_bottom':fields[1],
-                'vth_top':thermal[0],'vth_bottom':thermal[1],
-                'alpha':alpha,'beta':beta,'RIo':RIo}
-        
+    def _getParameters(self):
+        f = ff(join(self.prefix,'para.dat'))
+        r1 = f.readOther([  ('nx',np.int32),
+                            ('ny',np.int32),
+                            ('nz',np.int32),
+                            ('dx',np.float32),
+                            ('dy',np.float32),
+                            ('delz',np.float32)])
+        para = dict(zip(r1.dtype.names,r1[0]))
+
+        r2 = f.readOther([  ('nt',np.int32),
+                            ('dtsub_init',np.float32),
+                            ('ntsub',np.int32),
+                            ('dt',np.float32),
+                            ('nout',np.int32)])
+        para.update(zip(r2.dtype.names,r2[0]))
+
+        out_dir = f.readString()
+        para.update(('out_dir',out_dir))
+
+        r3 = f.readReals()
+        para.update(('vtop',r3[0]),('vbottom',r3[1]))
+
+        Ni_max = para.readInts()[0]
+        para.update(('Ni_max',Ni_max))
+
+        r4 = f.readOther([   ('mproton',np.float64),
+                            ('mpu',np.float64),
+                            ('mheavy',np.float32)])
+        para.update(zip(r4.dtype.names,r4[0]))
+
+        r5 = f.readReals()
+        para.update(('np_top',r5[0]),('np_bottom',r5[1]))
+
+        r6 = f.readReals()
+        para.update(('b0_top',r6[0]),('b0_bottom',r6[1]))
+
+        r7 = f.readReals()
+        para.update(('vth_top',r7[0]),('vth_bottom',r7[1]))
+
+        r8 = f.readOther([  ('alpha',np.float64),
+                            ('beta',np.float32)])
+        para.update(zip(r8.dtype.names,r8[0]))
+
+        RIo = f.readReals()[0]
+        para.update(('RIo',RIo))
+
+        return para
 
     def filenames(self):
         return [f for f in listdir(self.prefix) if isfile(join(self.prefix,f)) and self.var in f]
