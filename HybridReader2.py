124b4709234d990f2188a2c3e7ef6e482a765f12
Nathan Barnes <npbarnes@alaska.edu>
2016-07-11 12:14:59 -0800

    Change variable naming
    I made each record read go into a variable called record. Should be more
    readable.

--- HybridReader2.py
+++ HybridReader2.py
@@ -16,52 +16,52 @@ class HybridReader2:
         self.para = self._getParameters()
 
     def _getParameters(self):
-        # Read directly from para.dat
+        # Read from para.dat
         f = ff.FortranFile(join(self.prefix,'para.dat'))
-        r1 = f.readOther([  ('nx',np.int32),
+        record = f.readOther([  ('nx',np.int32),
                             ('ny',np.int32),
                             ('nz',np.int32),
                             ('dx',np.float32),
                             ('dy',np.float32),
                             ('delz',np.float32)])
-        para = dict(zip(r1.dtype.names,r1[0]))
+        para = dict(zip(record.dtype.names,record[0]))
 
-        r2 = f.readOther([  ('nt',np.int32),
+        record = f.readOther([  ('nt',np.int32),
                             ('dtsub_init',np.float32),
                             ('ntsub',np.int32),
                             ('dt',np.float32),
                             ('nout',np.int32)])
-        para.update(zip(r2.dtype.names,r2[0]))
+        para.update(zip(record.dtype.names,record[0]))
 
         out_dir = f.readString()
         para.update({'out_dir':out_dir})
 
-        r3 = f.readReals()
-        para.update([('vtop',r3[0]),('vbottom',r3[1])])
+        record = f.readReals()
+        para.update([('vtop',record[0]),('vbottom',record[1])])
 
-        Ni_max = f.readInts()[0]
-        para.update({'Ni_max':Ni_max})
+        record = f.readInts()
+        para.update({'Ni_max':record[0]})
 
-        r4 = f.readOther([  ('mproton',np.float64),
+        record = f.readOther([  ('mproton',np.float64),
                             ('mpu',np.float64),
                             ('mheavy',np.float32)])
-        para.update(zip(r4.dtype.names,r4[0]))
+        para.update(zip(record.dtype.names,record[0]))
 
-        r5 = f.readReals()
-        para.update([('np_top',r5[0]),('np_bottom',r5[1])])
+        record = f.readReals()
+        para.update([('np_top',record[0]),('np_bottom',record[1])])
 
-        r6 = f.readReals()
-        para.update([('b0_top',r6[0]),('b0_bottom',r6[1])])
+        record = f.readReals()
+        para.update([('b0_top',record[0]),('b0_bottom',record[1])])
 
-        r7 = f.readReals()
-        para.update([('vth_top',r7[0]),('vth_bottom',r7[1])])
+        record = f.readReals()
+        para.update([('vth_top',record[0]),('vth_bottom',record[1])])
 
-        r8 = f.readOther([  ('alpha',np.float64),
+        record = f.readOther([  ('alpha',np.float64),
                             ('beta',np.float32)])
-        para.update(zip(r8.dtype.names,r8[0]))
+        para.update(zip(record.dtype.names,record[0]))
 
-        RIo = f.readReals()[0]
-        para.update({'RIo':RIo})
+        record = f.readReals()
+        para.update({'RIo':record[0]})
 
         # Compute additional useful parameters
         paths = map(partial(join,self.grid),self.sort_filenames())
