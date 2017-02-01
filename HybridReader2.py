6793603c8a5c74b82eeac1e8aa82cce15e3aa862
Nathan Barnes <npbarnes@alaska.edu>
2017-01-31 16:56:24 -0900

    Current working version

--- HybridReader2.py
+++ HybridReader2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+from pprint import pprint
 import FortranFile as ff
 import numpy as np
 from os import SEEK_SET,SEEK_CUR,SEEK_END
@@ -9,15 +10,19 @@ from operator import itemgetter
 from functools import partial
 from HybridParams import HybridParams
 
+class HybridError(ValueError):
+    pass
+
 class HybridReader2:
     def __init__(self, prefix, variable):
         self.prefix = prefix
         self.grid = join(prefix,'grid')
         self.particle = join(prefix,'particle')
         self.var = variable
-        self.para = self._getParameters()
         self.paths = map(partial(join,self.grid),self.sort_filenames())
         self.handles = map(ff.FortranFile,self.paths)
+        self.para = self._getParameters()
+        self.isScalar = self._check_scalar()
 
     def _getParameters(self):
         """Reads parameters from para.dat and coords.dat and computes
@@ -26,7 +31,6 @@ class HybridReader2:
         para = HybridParams(self.prefix).para
 
         # Compute additional useful parameters
-        self.paths = map(partial(join,self.grid),self.sort_filenames())
         zrange = (para['nz']-2)*len(self.paths)
         saved_steps = para['nt']/para['nout']
         para.update({'zrange':zrange, 'saved_steps':saved_steps})
@@ -46,6 +50,21 @@ class HybridReader2:
 
     ######################################################################################
     # Utilities
+    def _check_scalar(self):
+        h = self.handles[0]
+        # skip timestep number
+        h.skipRecord()
+        size = h._read_check()
+        h.seek(0)
+        number = size/4
+        if number == self.para['nx']*self.para['ny']*self.para['nz']:
+            return True
+        elif number == self.para['nx']*self.para['ny']*self.para['nz']*3:
+            return False
+        else:
+            raise HybridError(str(number) + ' != ' + str(self.para['nx']*self.para['ny']*self.para['nz']))
+
+
     def filenames(self):
         return [f for f in listdir(self.grid) if isfile(join(self.grid,f)) and self.var in f
                                                                            and f.startswith('c.')]
@@ -68,8 +87,11 @@ class HybridReader2:
 
         return list(zip(*aug_list)[1])
 
-    def _cut_overlap(self,a):
+    def _scalar_cut_overlap(self,a):
         return a[:-2*self.para['nx']*self.para['ny']]
+
+    def _vector_cut_overlap(self,a):
+        return a[:-2*self.para['nx']*self.para['ny']*3]
     ###################################################################################
 
     def get_saved_timesteps(self):
@@ -90,17 +112,26 @@ class HybridReader2:
     def get_next_timestep(self):
         """Returns the next timestep number and data leaving the file position after that data"""
         # read the time step record
-        mrecords = map(lambda x: x.readReals(),self.handles)
+        mrecords = np.array(map(lambda x: x.readReals(),self.handles))
         assert mrecords.shape == (len(self.handles),1)
         assert np.all(mrecords == mrecords[0])
-        m = mrecrods[0]
-        # (xyz)
-        flat_data = np.concatenate(map(lambda x:self._cut_overlap(x.readReals()), self.handles))
-        # (x,y,z)
-        try:
-            data = np.reshape(flat_data,[self.para['nx'],self.para['ny'],(self.para['nz']-2)*len(self.paths)],'F')
-        except ValueError:
-            data = np.reshape(flat_data,[self.para['nx'],self.para['ny'],(self.para['nz']-2)*len(self.paths),3],'F')
+        m = mrecords[0]
+
+        if self.isScalar:
+            # (xyz)
+            flat_data = np.concatenate(map(lambda x:self._scalar_cut_overlap(x.readReals()), self.handles))
+            # (x,y,z)
+            data = np.reshape(flat_data,[self.para['nx'],self.para['ny'],self.para['zrange']],'F')
+        else:
+            # convert flattened data into 3d array of vectors
+            datalst = map(lambda x:x.readReals(), self.handles)
+            # shapes data from (p,xyzc) to (p,x,y,z,c)
+            redatalst = np.reshape(datalst,[len(self.handles),self.para['nx'],self.para['ny'],self.para['nz'],3], 'F')
+            cutOverlap = redatalst[:,:,:,:-2,:]
+            # (p,x,y,z,c) -> (x,y,p,z,c)
+            rolledlst = np.rollaxis(cutOverlap,0,3)
+            # (p,t,x,y,zrange,c)
+            data = np.reshape(rolledlst,[self.para['nx'],self.para['ny'],self.para['zrange'],3])
 
         return m, data
 
@@ -121,10 +152,11 @@ class HybridReader2:
         """Returns time step number, time, and data for the last saved step of the simulation"""
         map(lambda x:x.seek(0,SEEK_END), self.handles)
         m, data = self.get_prev_timestep()
+        map(lambda x:x.seek(0,SEEK_END), self.handles)
         return m, self.para['dt']*m, data
 
     def get_all_timesteps(self):
-        """Returns and array of structures containing step, time, and data for each saved timestep."""
+        """Returns and array of structures containing step, time, and data for each saved timestep"""
         steps = self.get_saved_timesteps()
         nx = self.para['nx']
         ny = self.para['ny']
@@ -134,12 +166,7 @@ class HybridReader2:
         ret = np.empty(len(steps),dtype=dtype)
 
         for n in range(len(steps)):
-            # First skip the time step record
-            map(lambda x: x.skipRecord(),self.handles)
-            # (xyz)
-            flat_data = np.concatenate(map(lambda x:self._cut_overlap(x.readReals()), self.handles))
-            # (x,y,z)
-            data = np.reshape(flat_data,[self.para['nx'],self.para['ny'],(self.para['nz']-2)*len(self.paths)],'F')
+            m, data = self.get_next_timestep()
 
             ret[n] = (steps[n],self.para['dt']*steps[n],data)
 
