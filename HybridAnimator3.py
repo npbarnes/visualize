888e66dd1ae915c9ba66aa280afb0813a534844f
Nathan Barnes <npbarnes@alaska.edu>
2021-11-15 14:43:32 -0900

    Only load slices for animations. Time text is broken.

--- HybridAnimator3.py
+++ HybridAnimator3.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import HybridHelper
 from HybridHelper import parser, parse_cmd_line, data_slice, direct_plot
+from collections import defaultdict
 
 # Animation specific arguments
 parser.add_argument('--framerate', type=int, default=20)
@@ -31,25 +32,26 @@ h = hr(args.prefix,args.variable.name)
 
 # Preload all the slices
 print(f"Loading {args.variable.name} data into memory")
-all_data = []
+all_data = defaultdict(list)
 all_steps = []
-for m, step_data in step_iter(h):
+for i, (m, step_data) in enumerate(step_iter(h)):
     if not h.isScalar:
         step_data = step_data[:,:,:,args.variable.coordinate]
     if args.variable.name == 'bt':
-        step_data = 1e9 * h.para['ion_amu'] * 1.6726219e-27/1.60217662e-19 * step_data # ion gyrofrequency -> nT
+        step_data *= 1e9 * h.para['ion_amu'] * 1.6726219e-27/1.60217662e-19 # ion gyrofrequency -> nT
     elif args.variable.name.startswith('np'):
         step_data *= (1e-3)**3 # km^-3 -> m^-3
     elif args.variable.name.startswith('up'):
         step_data *= 1e3 # km -> m
-    all_data.append(step_data)
+    if i == 0:
+        init_data_3d = step_data
+
+    for d in args.directions:
+        # Copy the slice, so that the larger 3-d array can be garbage collected
+        all_data[d].append(data_slice(h.para, step_data, d).copy())
     all_steps.append(m)
 
-dms = [mm - m for mm,m in zip(all_steps[1:], all_steps[:-1])]
 n_frames = len(all_steps)
-#n_frames = []
-#for i, dm in enumerate(dms):
-    #n_frames.extend([i] * dm)
 
 # Make the animations
 for (fig, ax), d in zip(make_figures(args), args.directions):
@@ -58,16 +60,19 @@ for (fig, ax), d in zip(make_figures(args), args.directions):
     ax.set_ylabel(f'{d[1].upper()} (km)', fontsize=20)
     ax.tick_params(labelsize=20)
     
-    if args.vmax is not None:
-        vmax = args.vmax
+    if args.norm is None:
+        if args.vmax is not None:
+            vmax = args.vmax
+        else:
+            vmax = max((np.max(dat) for dat in all_data[d]))
+        if args.vmin is not None:
+            vmin = args.vmin
+        else:
+            vmin = min((np.min(dat[dat!=0], initial=vmax/2) for dat in all_data[d]))
     else:
-        vmax = max((np.max(d) for d in all_data))
-    if args.vmin is not None:
-        vmin = args.vmin
-    else:
-        vmin = min((np.min(d[d!=0], initial=vmax/2) for d in all_data))
+        vmin, vmax = None, None
     # Make the inital plot
-    artist = direct_plot(fig, ax, all_data[0], h.para, d, cmap=args.colormap, norm=args.norm, vmin=vmin, vmax=vmax, mccomas=args.mccomas, skip_labeling=True)[0]
+    artist = direct_plot(fig, ax, init_data_3d, h.para, d, cmap=args.colormap, norm=args.norm, vmin=vmin, vmax=vmax, mccomas=args.mccomas, skip_labeling=True)[0]
     annotation = ax.annotate("...Placeholder String...",
             xy=(0.35, 0.975), xycoords='figure fraction',
             horizontalalignment='left', verticalalignment='top',
@@ -80,23 +85,19 @@ for (fig, ax), d in zip(make_figures(args), args.directions):
     qx,qy,qz = h.para['grid_points']
     cx = int(len(qx)/2)
     cz = int(len(qz)/2)
-    dots, = ax.plot([qx[cx-2],qx[cx-1],qx[cx],qx[cx+1],qx[cx+2]], 5*[qz[cz+5]], color='black', marker='o', markersize=0.7, linestyle='None')
+    if d == 'xz':
+        dots, = ax.plot([qx[cx-2],qx[cx-1],qx[cx],qx[cx+1],qx[cx+2]], 5*[qz[cz+5]], color='black', marker='o', markersize=0.7, linestyle='None')
     
     def update_animation(frame):
-        if frame == update_animation.prev_frame:
-            update_animation.prev_frame = frame
-            return ()
-        prev_frame = frame
-        s = data_slice(h.para, all_data[frame], d)
+        s = all_data[d][frame]
 
         # When changing the data for pcolormesh we need to remove the last element
         # in each direction for some reason.
         s = s[:-1, :-1]
 
         artist.set_array(s.T.ravel())
-        annotation.set_text(r"t = {:>4.1f} s".format(all_steps[frame]*h.para['dt']))
+        #annotation.set_text(r"t = {:>4.1f} s".format(all_steps[frame]*h.para['dt']))
         return artist,
-    update_animation.prev_frame = -1
     
     ani = animation.FuncAnimation(fig, frames=n_frames,
         func=update_animation, interval=100, blit=True)
