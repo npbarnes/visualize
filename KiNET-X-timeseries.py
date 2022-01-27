18593bf8ab96c129edd6767eddb9146d26d96d0c
Nathan Barnes <npbarnes@alaska.edu>
2022-01-27 12:10:13 -0900

    load from file, and refactor plotting.

--- KiNET-X-timeseries.py
+++ KiNET-X-timeseries.py
@@ -1,4 +1,4 @@
-#!/home/nathan/anaconda2/envs/jupyterlab/bin/python
+#!/usr/bin/env python
 import sys
 import numpy as np
 from matplotlib import rc
@@ -12,7 +12,7 @@ def getbobs(data):
     cx = nx//2
     cy = ny//2
     cz = nz//2
-    return data[(cx-2,cx-1,cx,cx+1,cx+2),cy-2,cz+5]
+    return data[(cx-2,cx-1,cx,cx+1,cx+2),cy,cz+5]
 
 def point_selection(data, mode):
     if mode is None:
@@ -48,6 +48,14 @@ def smootherator(arrs, n=3):
         ret[i,1:-1] = np.convolve(arr, np.ones((n,))/n, mode='valid')
         ret[i,-1] = (arr[-2] + arr[-1])/2
     return ret
+def smootherator_inplace(arrs, n=3):
+    ret = np.empty_like(arrs)
+    for i, arr in enumerate(arrs):
+        ret[i,0] = (arr[0]+arr[1])/2
+        ret[i,1:-1] = np.convolve(arr, np.ones((n,))/n, mode='valid')
+        ret[i,-1] = (arr[-2] + arr[-1])/2
+    arrs[:,:] = ret
+    return arrs
 
 def plot_each(ax, ts, arrs):
     for a, label in zip(arrs, ['-1000 m','-500 m','0 m','500 m','1000 m']):
@@ -59,72 +67,93 @@ q = 1.602e-19 # C
 m = 16*1.6726e-27 # kg
 q_over_m = q/m # C/kg
 
-if __name__ == "__main__":
-    ts1, total = timeseries_lowmemory('np_tot')
-    ts2, oxygen = timeseries_lowmemory('np_H')
-    ts3, barium = timeseries_lowmemory('np_CH4')
-    ts4, B = timeseries_lowmemory('bt', mode='mag')
-    ts5, Bx = timeseries_lowmemory('bt', mode='x')
-    ts6, u = timeseries_lowmemory('up', mode='mag')
-    ts7, E = timeseries_lowmemory('E', mode='mag')
-    ts8, T_i = timeseries_lowmemory('temp_p')
+def fig_axs_setup():
+    fig, axs = plt.subplots(nrows=8, ncols=1, sharex=True, figsize=(8.5,11))
+    fig.subplots_adjust(left=0.15, right=0.85, bottom=0.05, top=0.96, wspace=0, hspace=0.25)
 
-    assert np.array_equal(ts1,ts2)
-    assert np.array_equal(ts1,ts3)
-    assert np.array_equal(ts1,ts4)
-    assert np.array_equal(ts1,ts5)
-    assert np.array_equal(ts1,ts6)
-    assert np.array_equal(ts1,ts7)
-    assert np.array_equal(ts1,ts8)
-    ts = ts1
+    axs[0].set_title('Plasma properties timeseries')
 
-    # some profiles benefit from smoothing
-    oxygen = smootherator(oxygen)
-    T_i = smootherator(T_i)
+    axs[0].set_ylabel('$n$\n$(\mathrm{m}^{-3})$', rotation=0, labelpad=20, multialignment='center')
+    axs[1].set_ylabel('$n_O$\n$(\mathrm{m}^{-3})$', rotation=0, labelpad=20, multialignment='center')
+    axs[2].set_ylabel('$n_{Ba}$\n$(\mathrm{m}^{-3})$', rotation=0, labelpad=20, multialignment='center')
+    axs[3].set_ylabel('$|B|$\n$(\mathrm{nT})$', rotation=0, labelpad=20, multialignment='center')
+    axs[4].set_ylabel('$B_x$\n$(\mathrm{nT})$', rotation=0, labelpad=20, multialignment='center')
+    axs[5].set_ylabel('$|u|$\n$(\mathrm{m}/\mathrm{s})$', rotation=0, labelpad=20, multialignment='center')
+    axs[6].set_ylabel('$|E|$\n$(\mathrm{\mu V}/\mathrm{m})$', rotation=0, labelpad=20, multialignment='center')
+    axs[7].set_ylabel('$\mathrm{T}_i$\n(eV)', rotation=0, labelpad=20, multialignment='center')
 
-    # B needs to be converted to nT, and E to micro V/m
+    axs[-1].set_xlabel('seconds since release')
+
+    return fig, axs
+
+def plot_all(fig, axs, ts, timeseries_lst):
+    for ax, timeseries in zip(axs, timeseries_lst):
+        plot_each(ax, ts, timeseries)
+
+    l = axs[2].legend(title='Approx Offset\n(+ upstream)', bbox_to_anchor=(1.001,1), loc='upper left', fontsize=10)
+    plt.setp(l.get_title(), multialignment='center')
+
+    plt.show()
+
+def unique(ar):
+    """Similar to numpy.unique with return_index=True.
+    This function returns the unique values and last indices where a value occurs (rather than 
+    the first indices). There's no option to not return indices, just use np.unique in that case.
+    There's no option for inverse or counts, sorry.
+    """
+    perm = ar.argsort(kind='stable')
+    aux = ar[perm]
+    mask = np.empty(aux.shape, dtype=bool)
+    mask[-1] = True
+    mask[:-1] = aux[:-1] != aux[1:]
+    return aux[mask], perm[mask]
+
+def load_and_prep():
+    data = np.load("timeseries.npz")
+    ts = data["ts"]
+    total = data["total"]
+    oxygen = data["oxygen"]
+    barium = data["barium"]
+    B = data["B"]
+    Bx = data["Bx"]
+    u = data["u"]
+    E = data["E"]
+    T_i = data["T_i"]
+
+    ts = ts.reshape(len(ts))
+    ts,idxs = unique(ts)
+    total  = total[:, idxs]
+    oxygen = oxygen[:, idxs]
+    barium = barium[:, idxs]
+    B      = B[:, idxs]
+    Bx     = Bx[:, idxs]
+    u      = u[:, idxs]
+    E      = E[:, idxs]
+    T_i    = T_i[:, idxs]
+
+    smootherator_inplace(total)
+    smootherator_inplace(oxygen)
+    smootherator_inplace(barium)
+    smootherator_inplace(B)
+    smootherator_inplace(Bx)
+    smootherator_inplace(u)
+    smootherator_inplace(E)
+    smootherator_inplace(T_i)
+
+    # Units
     B  *= 1e9/q_over_m # nT
     Bx *= 1e9/q_over_m # nT
     E  *= 1e6/q_over_m # micro V/m
 
     mpkm = 1000
+    total  /= mpkm**3 # m^-3
+    oxygen /= mpkm**3 # m^-3
+    barium /= mpkm**3 # m^-3
+    u      *= mpkm    # m/s
 
-    #######################################################################################################
-    fig, axs = plt.subplots(nrows=8, ncols=1, sharex=True, figsize=(8,8/7*10))
-    fig.subplots_adjust(left=0.15, right=0.85, bottom=0.05, top=0.95, wspace=0, hspace=0)
+    return ts, total, oxygen, barium, B, Bx, u, E, T_i
 
-    plot_each(axs[0], ts, total/mpkm**3)
-    axs[0].set_ylabel('$n$\n$(\mathrm{m}^{-3})$', rotation=0, labelpad=20, multialignment='center')
-    locs = axs[0].get_yticks()
-    axs[0].set_yticklabels((f"${x:.1f} \\times 10^{{12}}$" for x in locs*1e-12))
-
-    plot_each(axs[1], ts, oxygen/mpkm**3)
-    axs[1].set_ylabel('$n_O$\n$(\mathrm{m}^{-3})$', rotation=0, labelpad=20, multialignment='center')
-    locs = axs[1].get_yticks()
-    axs[1].set_yticklabels((f"${x:.1f} \\times 10^{{12}}$" for x in locs*1e-12))
-
-    plot_each(axs[2], ts, barium/mpkm**3)
-    axs[2].set_ylabel('$n_{Ba}$\n$(\mathrm{m}^{-3})$', rotation=0, labelpad=20, multialignment='center')
-    locs = axs[2].get_yticks()
-    axs[2].set_yticklabels((f"${x:.1f} \\times 10^{{12}}$" for x in locs*1e-12))
-
-    plot_each(axs[3], ts, B)
-    axs[3].set_ylabel('$|B|$\n$(\mathrm{nT})$', rotation=0, labelpad=20, multialignment='center')
-
-    plot_each(axs[4], ts, Bx)
-    axs[4].set_ylabel('$B_x$\n$(\mathrm{nT})$', rotation=0, labelpad=20, multialignment='center')
-
-    plot_each(axs[5], ts, u*mpkm)
-    axs[5].set_ylabel('$|u|$\n$(\mathrm{m}/\mathrm{s})$', rotation=0, labelpad=20, multialignment='center')
-
-    plot_each(axs[6], ts, E)
-    axs[6].set_ylabel('$|E|$\n$(\mathrm{\mu V}/\mathrm{m})$', rotation=0, labelpad=20, multialignment='center')
-
-    plot_each(axs[7], ts, T_i)
-    axs[7].set_ylabel('$\mathrm{T}_i$\n(eV)', rotation=0, labelpad=20, multialignment='center')
-
-    axs[0].set_title('Plasma properties timeseries')
-    l = axs[2].legend(title='Offset\n(+ upstream)', bbox_to_anchor=(1.001,1), loc='upper left', fontsize=10)
-    plt.setp(l.get_title(), multialignment='center')
-    axs[-1].set_xlabel('seconds since release')
-    plt.show()
+if __name__ == "__main__":
+    fig, axs = fig_axs_setup()
+    ts, *timeseries_lst = load_and_prep()
+    plot_all(fig, axs, ts, timeseries_lst)
